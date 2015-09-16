74d90553b371413308097e6333d02c09d17ae987
Couscous auto deploy <couscous@couscous.io>
2015-09-16 18:49:37 +0000

    Website generation with Couscous

--- js/randimg.js
+++ js/randimg.js
@@ -4,26 +4,6 @@
 
 (function() {
     var sources = [
-        "http://1339.cf/img/1.png",
-        "http://1339.cf/img/2.png",
-        "http://1339.cf/img/3.png",
-        "http://1339.cf/img/4.png",
-        "http://1339.cf/img/5.png",
-        "http://1339.cf/img/6.png",
-        "http://1339.cf/img/7.png",
-        "http://1339.cf/img/8.png",
-        "http://1339.cf/img/9.png",
-        "http://1339.cf/img/10.png",
-        "http://1339.cf/img/11.png",
-        "http://1339.cf/img/12.png",
-        "http://1339.cf/img/13.png",
-        "http://1339.cf/img/14.png",
-        "http://1339.cf/img/15.png",
-        "http://1339.cf/img/16.png",
-        "http://1339.cf/img/17.png",
-        "http://1339.cf/img/18.png",
-        "http://1339.cf/img/19.png",
-        "http://1339.cf/img/20.png"
     ];
 
     var Apply = function() {
@@ -53,8 +33,8 @@
     var sl = document.createElement('img');
     var vx;
 
-	sl.className = 'gril';
-	sl.alt = 'qtgril';
+	sl.className = 'img-sticky';
+	sl.alt = 'random image';
 
     // Apply the element to the body. You can have it appended to a custom
     // element by changing 'document.body' to the requested DOM element.
